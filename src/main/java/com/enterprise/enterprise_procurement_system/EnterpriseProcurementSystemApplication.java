86ca9c647294c13e09963adcd3bcdbf12b835621
B Nithin Kumar <bnithinkumar12345@gmail.com>
2026-07-21 20:56:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/enterprise/enterprise_procurement_system/EnterpriseProcurementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.enterprise_procurement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseProcurementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseProcurementSystemApplication.class, args);
+	}
+
+}
